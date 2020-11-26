c74f80ce9e80fd3e185810149ef8fcb57c94994c
nikhilbalne <nikhil.b@ecoihub.com>
2020-11-26 19:41:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/qurix/haptic_feedback/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qurix.haptic_feedback
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vibrate Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text('Haptic Feedback'),
+          onPressed: () {
+            HapticFeedback.vibrate();
+          },
+          color: Colors.black12,
+        ),
+      ),
+    );
+  }
+}
